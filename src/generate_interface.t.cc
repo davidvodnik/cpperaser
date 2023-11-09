17f87367f218825dd0a1be89e659609608a17c6f
David Vodnik <davidvodnik@gmail.com>
2023-11-09 11:51:45 +0100

    Add tests for generate_interface

--- src/generate_interface.t.cc
+++ src/generate_interface.t.cc
@@ -1,2 +1,81 @@
 #include <catch.hpp>
 #include <generate_interface.h>
+
+using namespace CppEraser;
+
+TEST_CASE("Parse interface") {
+    Interface interface {
+        "Drawable", {
+            { {false, "int"}, "draw", {}, false }
+        }
+    };
+    auto generated = generate_interface(interface, false);
+    REQUIRE(generated == R"(#include <memory>
+
+class Drawable {
+
+    struct concept_ {
+        virtual ~concept_() = default;
+        virtual int draw_() = 0;
+    };
+
+    template <typename T> struct model_ : concept_ {
+        model_(const T& t) : value_(t) {}
+        int draw_() override { return value_.draw(); }
+
+        T value_;
+    };
+
+public:
+    template <typename T> Drawable(const T &t) {
+        value_ = std::make_unique<model_<T>>(t);
+    }
+
+    int draw() { return value_->draw_(); }
+
+private:
+    std::unique_ptr<concept_> value_;
+};
+)");
+}
+
+TEST_CASE("Parse interface - copyable") {
+    Interface interface {
+        "Drawable", {
+            { {false, "int"}, "draw", {}, false }
+        }
+    };
+    auto generated = generate_interface(interface, true);
+    REQUIRE(generated == R"(#include <memory>
+
+class Drawable {
+
+    struct concept_ {
+        virtual ~concept_() = default;
+        virtual std::unique_ptr<concept_> copy_() const = 0;
+        virtual int draw_() = 0;
+    };
+
+    template <typename T> struct model_ : concept_ {
+        model_(const T& t) : value_(t) {}
+        std::unique_ptr<concept_> copy_() const override {
+            return std::make_unique<model_>(value_);
+        }
+        int draw_() override { return value_.draw(); }
+
+        T value_;
+    };
+
+public:
+    template <typename T> Drawable(const T &t) {
+        value_ = std::make_unique<model_<T>>(t);
+    }
+    Drawable(const Drawable& other) : value_(other.value_->copy_()) {}
+
+    int draw() { return value_->draw_(); }
+
+private:
+    std::unique_ptr<concept_> value_;
+};
+)");
+}
