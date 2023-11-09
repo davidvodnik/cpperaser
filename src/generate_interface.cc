396d5329f879e82925432173749ef77287b34f0a
David Vodnik <davidvodnik@gmail.com>
2023-11-09 16:38:31 +0100

    Restructure generate_interface to prevent having a full template per case

--- src/generate_interface.cc
+++ src/generate_interface.cc
@@ -1,160 +1,143 @@
 #include <fmt/core.h>
 #include <generate_interface.h>
+#include <numeric>
 
 namespace CppEraser {
+
+std::string
+generate_arguments(const std::vector<CppEraser::Parameter> &parameters) {
+    return std::accumulate(
+        begin(parameters), end(parameters), std::string(), [](auto s, auto &p) {
+            return (s.empty() ? "" : (s + ", ")) + p.type.name + " " + p.name;
+        });
+}
+
+std::string
+generate_parameters(const std::vector<CppEraser::Parameter> &parameters) {
+    return std::accumulate(
+        begin(parameters), end(parameters), std::string(),
+        [](auto s, auto &p) { return (s.empty() ? "" : (s + ", ")) + p.name; });
+}
+
+constexpr auto concept_template =
+    R"(    struct concept_ {{
+        virtual ~concept_() = default;{0}
+    }};)";
+constexpr auto concept_copy_template =
+    "\n        virtual std::unique_ptr<concept_> copy_() const = 0;";
+constexpr auto concept_method_template = "\n        virtual {} {}_({}){} = 0;";
+
+std::string
+generate_concept_methods(const std::vector<CppEraser::Method> &methods,
+                         bool copyable) {
+    return fmt::format(
+        concept_template,
+        std::accumulate(
+            begin(methods), end(methods),
+            std::string(copyable ? concept_copy_template : ""),
+            [](auto s, auto &method) {
+                return s + fmt::format(concept_method_template,
+                                       method.type.name, method.name,
+                                       generate_arguments(method.parameters),
+                                       method.constant ? " const" : "");
+            }));
+}
+
+constexpr auto model_template =
+    R"(    template <typename T> struct model_ : concept_ {{
+        model_(const T& t) : value_(t) {{}}{0}
+
+        T value_;
+    }};)";
+constexpr auto model_copy_template =
+    R"(
+        std::unique_ptr<concept_> copy_() const override {
+            return std::make_unique<model_>(value_);
+        })";
+
+std::string
+generate_model_methods(const std::vector<CppEraser::Method> &methods,
+                       bool copyable) {
+    return fmt::format(
+        model_template,
+        std::accumulate(
+            begin(methods), end(methods),
+            std::string(copyable ? model_copy_template : ""),
+            [](auto s, auto &method) {
+                auto arguments = generate_arguments(method.parameters);
+                auto parameters = generate_parameters(method.parameters);
+                auto ret = method.type.name == "void" ? "" : "return ";
+                if (method.free_function) {
+                    return s +
+                           fmt::format("\n        {0} {1}_({2}){6} override {{ "
+                                       "{4}{1}(value_{5}{3}); }}",
+                                       method.type.name, method.name, arguments,
+                                       parameters, ret,
+                                       !parameters.empty() ? ", " : "",
+                                       method.constant ? " const" : "");
+                } else {
+                    return s +
+                           fmt::format("\n        {0} {1}_({2}){5} override {{ "
+                                       "{4}value_.{1}({3}); }}",
+                                       method.type.name, method.name, arguments,
+                                       parameters, ret,
+                                       method.constant ? " const" : "");
+                }
+            }));
+}
+
+constexpr auto methods_template =
+    R"(public:
+    template <typename T> {0}(const T &t) {{
+        value_ = std::make_unique<model_<T>>(t);
+    }}
+{1})";
+constexpr auto methods_copy_template =
+    R"(    {0}(const {0}& other) : value_(other.value_->copy_()) {{}}
+)";
+
+std::string generate_interface_methods(Interface interface, bool copyable) {
+    return fmt::format(
+        methods_template, interface.name,
+        std::accumulate(
+            begin(interface.methods), end(interface.methods),
+            std::string(copyable
+                            ? fmt::format(methods_copy_template, interface.name)
+                            : ""),
+            [](auto s, auto &method) {
+                return s +
+                       fmt::format(
+                           "\n    {0} {1}({2}){5} {{ {4}value_->{1}_({3}); }}",
+                           method.type.name, method.name,
+                           generate_arguments(method.parameters),
+                           generate_parameters(method.parameters),
+                           method.type.name == "void" ? "" : "return ",
+                           method.constant ? " const" : "");
+            }));
+}
 
 constexpr auto interface_template =
     R"(#include <memory>
 
 class {0} {{
 
-    struct concept_ {{
-        virtual ~concept_() = default;{1}
-    }};
+{1}
 
-    template <typename T> struct model_ : concept_ {{
-        model_(const T& t) : value_(t) {{}}{2}
+{2}
 
-        T value_;
-    }};
-
-public:
-    template <typename T> {0}(const T &t) {{
-        value_ = std::make_unique<model_<T>>(t);
-    }}
 {3}
 
 private:
     std::unique_ptr<concept_> value_;
 }};
 )";
-
-constexpr auto interface_template_copyable =
-    R"(#include <memory>
-
-class {0} {{
-
-    struct concept_ {{
-        virtual ~concept_() = default;
-        virtual std::unique_ptr<concept_> copy_() const = 0;{1}
-    }};
-
-    template <typename T> struct model_ : concept_ {{
-        model_(const T& t) : value_(t) {{}}
-        std::unique_ptr<concept_> copy_() const override {{
-            return std::make_unique<model_>(value_);
-        }}{2}
-
-        T value_;
-    }};
-
-public:
-    template <typename T> {0}(const T &t) {{
-        value_ = std::make_unique<model_<T>>(t);
-    }}
-    {0}(const {0}& other) : value_(other.value_->copy_()) {{}}
-{3}
-
-private:
-    std::unique_ptr<concept_> value_;
-}};
-)";
-
-std::string
-generate_arguments(const std::vector<CppEraser::Parameter> &parameters) {
-    std::string argument_list;
-    for (auto parameter = begin(parameters); parameter != end(parameters);) {
-        argument_list +=
-            fmt::format("{} {}", parameter->type.name, parameter->name);
-        if (++parameter == end(parameters))
-            break;
-
-        argument_list += ", ";
-    }
-    return argument_list;
-}
-
-std::string
-generate_parameters(const std::vector<CppEraser::Parameter> &parameters) {
-    std::string parameter_list;
-    for (auto parameter = begin(parameters); parameter != end(parameters);) {
-        parameter_list += fmt::format("{}", parameter->name);
-        if (++parameter == end(parameters))
-            break;
-
-        parameter_list += ", ";
-    }
-    return parameter_list;
-}
-
-std::string
-generate_concept_methods(const std::vector<CppEraser::Method> &methods) {
-    std::string concept_methods;
-    for (auto method = begin(methods); method != end(methods); ++method) {
-        auto arguments = generate_arguments(method->parameters);
-        concept_methods += fmt::format(
-            "\n        virtual {} {}_({}){} = 0;", method->type.name,
-            method->name, arguments, method->constant ? " const" : "");
-    }
-    return concept_methods;
-}
-
-std::string
-generate_model_methods(const std::vector<CppEraser::Method> &methods) {
-    std::string model_methods;
-    for (auto method = begin(methods); method != end(methods); ++method) {
-        auto arguments = generate_arguments(method->parameters);
-        auto parameters = generate_parameters(method->parameters);
-        auto ret = method->type.name == "void" ? "" : "return ";
-        if (method->free_function) {
-            model_methods +=
-                fmt::format("\n        {0} {1}_({2}){6} override {{ "
-                            "{4}{1}(value_{5}{3}); }}",
-                            method->type.name, method->name, arguments,
-                            parameters, ret, !parameters.empty() ? ", " : "",
-                            method->constant ? " const" : "");
-        } else {
-            model_methods += fmt::format(
-                "\n        {0} {1}_({2}){5} override {{ {4}value_.{1}({3}); }}",
-                method->type.name, method->name, arguments, parameters, ret,
-                method->constant ? " const" : "");
-        }
-    }
-    return model_methods;
-}
-
-std::string
-generate_interface_methods(const std::vector<CppEraser::Method> &methods) {
-    std::string interface_methods;
-    for (auto method = begin(methods); method != end(methods); ++method) {
-        auto arguments = generate_arguments(method->parameters);
-        auto parameters = generate_parameters(method->parameters);
-        auto ret = method->type.name == "void" ? "" : "return ";
-        interface_methods +=
-            fmt::format("\n    {0} {1}({2}){5} {{ {4}value_->{1}_({3}); }}",
-                        method->type.name, method->name, arguments, parameters,
-                        ret, method->constant ? " const" : "");
-    }
-    return interface_methods;
-}
 
 std::string generate_interface(const CppEraser::Interface &interface,
                                bool copyable) {
-
-    std::string concept_methods = generate_concept_methods(interface.methods);
-
-    std::string model_methods = generate_model_methods(interface.methods);
-
-    std::string interface_methods =
-        generate_interface_methods(interface.methods);
-
-    if (copyable) {
-        return fmt::format(interface_template_copyable, interface.name,
-                           concept_methods, model_methods, interface_methods);
-    } else {
-        return fmt::format(interface_template, interface.name, concept_methods,
-                           model_methods, interface_methods);
-    }
+    return fmt::format(interface_template, interface.name,
+                       generate_concept_methods(interface.methods, copyable),
+                       generate_model_methods(interface.methods, copyable),
+                       generate_interface_methods(interface, copyable));
 }
 
 } // namespace CppEraser
